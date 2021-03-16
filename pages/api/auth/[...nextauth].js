de4dd14748c4728eaace786e55c6e200c41b1570
Ronan <roleary81@gmail.com>
2021-03-16 02:19:41 +0100

    oops

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,10 @@ import Providers from 'next-auth/providers'
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: 'Iv1.c827cc7979700200',
-      clientSecret: '739d2a3a6e10a366414fa0680759b1598c902ade'
-    }),
+    // Providers.GitHub({
+    //   clientId: 'Iv1.c827cc7979700200',
+    //   clientSecret: '739d2a3a6e10a366414fa0680759b1598c902ade'
+    // }),
     Providers.Credentials({
       name: 'Credentials',
       credentials: {
